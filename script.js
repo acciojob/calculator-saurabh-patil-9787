a41737004c00657644a647ee0c0671de7493cdf0
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-05 10:35:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your code here
+const display = document.getElementById('input')
+const buttons = document.querySelectorAll('button')
+const ans = document.getElementById('ans')
+const specialChar = ["+","-","/","*","="]
+let output = '';
+
+const calculate = (btnvalue)=>{
+       if(btnvalue==="=" && output !== ""){
+        output = eval(output)
+       }
+      else if(btnvalue === "C" ){
+           output = "";
+       }
+      
+       else{
+        if(output==="" && specialChar.includes(btnvalue)) return;
+        output+=btnvalue
+
+       }
+       display.value = output
+}
+
+buttons.forEach((button)=>{
+    
+    button.addEventListener('click',(event)=>calculate(event.target.innerText))
+   
+})
+
+
